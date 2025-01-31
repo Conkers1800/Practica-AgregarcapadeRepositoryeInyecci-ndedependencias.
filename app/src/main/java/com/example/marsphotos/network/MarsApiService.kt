6b6b00cb756375cb9549c81696b6945e0707b2f8
Conkers1800 <diaz.1800xd@gmail.com>
2025-01-30 20:25:16 -0600

    Correcciones de apartado 4 y codigo agregado de api.

--- app/src/main/java/com/example/marsphotos/network/MarsApiService.kt
+++ app/src/main/java/com/example/marsphotos/network/MarsApiService.kt
@@ -17,10 +17,6 @@
 package com.example.marsphotos.network
 
 import com.example.marsphotos.model.MarsPhoto
-import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
-import kotlinx.serialization.json.Json
-import okhttp3.MediaType.Companion.toMediaType
-import retrofit2.Retrofit
 import retrofit2.http.GET
 
     private const val BASE_URL =
@@ -29,10 +25,6 @@ import retrofit2.http.GET
     /**
      * Use the Retrofit builder to build a retrofit object using a kotlinx.serialization converter
      */
-    private val retrofit = Retrofit.Builder()
-        .addConverterFactory(Json.asConverterFactory("application/json".toMediaType()))
-        .baseUrl(BASE_URL)
-        .build()
 
     /**
      * Retrofit service object for creating api calls
@@ -45,8 +37,3 @@ import retrofit2.http.GET
     /**
      * A public Api object that exposes the lazy-initialized Retrofit service
      */
-    object MarsApi {
-        val retrofitService: MarsApiService by lazy {
-            retrofit.create(MarsApiService::class.java)
-        }
-}
